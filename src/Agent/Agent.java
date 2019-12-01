dd0a9a635c8d873d05f188057de9eb6abaaf1aad
Aidan O'Hara <aidano98@unm.edu>
2019-12-01 16:04:29 -0700

    Started methods to connect to bank

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,17 +1,21 @@
 package Agent;
 
+import shared.Message;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.LinkedList;
+import java.util.UUID;
 
 public class Agent {
 
 
-    private ServerSocket BankServer = null;
-    private ServerSocket AuctionServer = null;
-    private Socket client = null;
+    private ServerSocket bankServer = null;
+    private ServerSocket auctionServer = null;
+    private Socket bankClient = null;
+    private Socket auctionClient = null;
 
     private ObjectInputStream bankIn;
     private ObjectOutputStream bankOut;
@@ -20,23 +24,47 @@ public class Agent {
 
     public boolean connectedToBank;
     private double balance;
-    private int accountNumber;
+    private UUID accountNumber;
     //add a list of items the agent has won.
+    private String auctionHouse;
+    private int auctionPort;
 
 
     public Agent(String hostName, int portNumber) {
         try {
-            client = new Socket(hostName,portNumber);
+            bankClient = new Socket(hostName,portNumber);
             System.out.println("Connected");
-            bankOut = new ObjectOutputStream(client.getOutputStream());
+            bankOut = new ObjectOutputStream(bankClient.getOutputStream());
         } catch(IOException u) {
             u.printStackTrace();
         }
     }
 
-    public void registerBank() {
+    public void registerBank() throws IOException {
+        // create a message and send it to the bank using the message class...
+    Message.Builder register = new Message.Builder();
+    register.command(Message.Command.REGISTER_CLIENT);
+    Message registerMessage = new Message(register);
+    bankOut.writeObject(registerMessage);
+    new setBankIn();
+    // need a new thread here right?
+    }
 
+    public class setBankIn implements Runnable{
+        public Message message;
+        @Override
+        public void run() {
+            System.out.println("listening to bank");
+            try {
+                bankIn = new ObjectInputStream(bankClient.getInputStream());
+                message = (Message) bankIn.readObject(); //?
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        // need too confer with magnus about how to use message to read in the UUID
 
+        //add methods for different bank messages right?
     }
 
 
@@ -56,11 +84,23 @@ public class Agent {
 
     }
 
-    public boolean connectToAuctionHouse(int choice) {
-        hostName
-        try (Socket socket = new Socket(hostName, portNumber);
+    public boolean connectToAuctionHouse(int choice) throws IOException {
+        getAuctionNetInfo(choice);
+        try {
+            auctionClient = new Socket(auctionHouse,auctionPort);
 
-        return false;
+            auctionServer = new ServerSocket(auctionServerPort);
+
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getAuctionNetInfo(int choice) {
     }
 
     public AuctionENUMs makeBid(/*Item to bid on and price?*/) {
